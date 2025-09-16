3b91a816d5254116ff22c9c9a36dfaa7804fcb4f
amdaniamin <amdaniamin52@gmail.com>
2025-09-16 20:21:37 +0700

    3Load.lua

---
+++ 3Load.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/amdaniamin/Script.lua-Roblox/main/WalkingBot.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/amdaniamin/Script.lua-Roblox/main/NOT2.lua"))()
+
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-YARHM-12403"))()
